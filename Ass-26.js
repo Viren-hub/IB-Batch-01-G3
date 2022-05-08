14b3da09069d5bd448be483a4a05912182a5396d
Rohit-Mali <rohitamali123@gmail.com>
2022-05-08 17:02:57 +0530

    "initial commit"

--- Ass-26.js
+++ Ass-26.js
@@ -21,69 +21,71 @@ fetch('https://api.covid19api.com/dayone/country/india').then((data) => {
 
     objectData.map((values) => {
         tableData += `<tr>
-        
+
         <td data-aos="fade-up">${values.ID}</td>
-        <td data-aos="fade-down">${values.Country}</td>
-        <td data-aos="fade-right">${values.CountryCode}</td>
-        <td data-aos="fade-up-left">${values.Provience}</td>
-        <td data-aos="fade-down-right">${values.Lat}</td>
-        <td data-aos="slide-up">${values.Lon}</td>
-        <td data-aos="slide-left">${values.Confirmed}</td>
-        <td data-aos="zoom-out">${values.Deaths}</td>
-        <td data-aos="fade-up-left">${values.Recovered}</td>
-        <td data-aos="slide-down">${values.Active}</td>
-        <td data-aos="fade-down">${new Date(values.Date).toLocaleDateString()}</td>
-    </tr>`;
+         <td data-aos="fade-down">${values.Country}</td>
+         <td data-aos="fade-right">${values.CountryCode}</td>
+         <td data-aos="fade-up-left">${values.Provience}</td>
+         <td data-aos="fade-down-right">${values.Lat}</td>
+         <td data-aos="slide-up">${values.Lon}</td>
+         <td data-aos="slide-left">${values.Confirmed}</td>
+         <td data-aos="zoom-out">${values.Deaths}</td>
+         <td data-aos="fade-up-left">${values.Recovered}</td>
+         <td data-aos="slide-down">${values.Active}</td>
+         <td data-aos="fade-down">${new Date(values.Date).toLocaleDateString()}</td>
+     </tr>`;
 
     });
+
     document.getElementById("tablebody").innerHTML = tableData;
-});
-
-function call(name) {
-    fetch(`https://api.covid19api.com/dayone/country/${name}`).then((data) => {
-
-        return data.json();
-    }).then((objectData) => {
-        console.log(objectData[0].ID);
-
-
-        let tableData = "";
-
-        objectData.map((values) => {
-            tableData += `<tr>
-        
-        <td data-aos="fade-up">${values.ID}</td>
-        <td data-aos="fade-down">${values.Country}</td>
-        <td data-aos="fade-right">${values.CountryCode}</td>
-        <td data-aos="fade-up-left">${values.Provience}</td>
-        <td data-aos="fade-down-right">${values.City}</td>
-        <td data-aos="flip-up">${values.CityCode}</td>
-        <td data-aos="fade-down-right">${values.Lat}</td>
-        <td data-aos="slide-up">${values.Lon}</td>
-        <td data-aos="slide-left">${values.Confirmed}</td>
-        <td data-aos="zoom-out">${values.Deaths}</td>
-        <td data-aos="fade-up-left">${values.Recovered}</td>
-        <td data-aos="slide-down">${values.Active}</td>
-        <td data-aos="fade-down">${new Date(values.Date).toLocaleDateString()}</td>
-    </tr>`;
-
+    $(document).ready(function () {
+        $('.table').DataTable({
+            "order": [[10, "desc"]]
         });
-         document.getElementById("tablebody").innerHTML = tableData;
-        // document.getElementById('tablebody').innerHTML = tableData;
-        // $(document).ready(function () {
-        //     $('.tab').dataTables({
-        //         "order": [[10, "desc"]]
-    
-        //     });
-        // }); 
     });
+})
 
-}
-// function nextPage(){
-// document.getElementById('tablebody').innerHTML = tablebody;
-//                 $(document).ready(function() {
-//                     $('tablebody').DataTable( {
-//                         "order": [[ 10, "desc" ]]
-//                     } );
-//                 } );
-//             }
+
+
+    function call(name) {
+        fetch(`https://api.covid19api.com/dayone/country/${name}`).then((data) => {
+
+            return data.json();
+        }).then((objectData) => {
+            console.log(objectData[0].ID);
+
+
+            let tableData = "";
+
+            objectData.map((values) => {
+                tableData += `<tr>
+
+         <td data-aos="fade-up">${values.ID}</td>
+         <td data-aos="fade-down">${values.Country}</td>
+         <td data-aos="fade-right">${values.CountryCode}</td>
+         <td data-aos="fade-up-left">${values.Provience}</td>
+         <td data-aos="fade-down-right">${values.City}</td>
+         <td data-aos="flip-up">${values.CityCode}</td>
+         <td data-aos="fade-down-right">${values.Lat}</td>
+         <td data-aos="slide-up">${values.Lon}</td>
+         <td data-aos="slide-left">${values.Confirmed}</td>
+         <td data-aos="zoom-out">${values.Deaths}</td>
+         <td data-aos="fade-up-left">${values.Recovered}</td>
+         <td data-aos="slide-down">${values.Active}</td>
+         <td data-aos="fade-down">${new Date(values.Date).toLocaleDateString()}</td>
+     </tr>`;
+
+            });
+            document.getElementById("tablebody").innerHTML = tableData;
+            //  $(document).ready(function () {
+            //      $('.table').DataTable({
+            //          "order": [[10, "desc"]]
+            //      });
+            //  });
+        });
+
+    }
+
+
+
+
